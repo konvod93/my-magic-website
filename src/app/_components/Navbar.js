f6d72850e7dec1f48a9340479fef5de8df77e421
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-01-02 17:29:57 +0200

    updated Navbar

--- src/app/_components/Navbar.js
+++ src/app/_components/Navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Navbar = () => {
   return (
     <div className="w-full">
-        <div className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
+        <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
             <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
                 <Link href="/">
                     <span className="flex items-center space-x-2 text-2xl font-medium text-gray-100">
@@ -11,7 +11,18 @@ const Navbar = () => {
                     </span>
                 </Link>
             </div>
-        </div>
+
+            <div className="hidden mr-3 space-x-4 lg:flex nav_item">
+                <Link href="/" locale="ru">
+                    Russian
+                </Link>
+                <Link href="/" locale="en">
+                    In English
+                </Link>
+                {/* <BookButton btnType="small">{t("cta.btn.text")}</BookButton> */}
+                <Link href="#contact" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">Book me!</Link>
+            </div>
+        </nav>
     </div>
   )
 }
