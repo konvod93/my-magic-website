ed9103b4bf3f3cd656b49452d5cc678cf29e74c1
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-01-02 17:06:55 +0200

    updating Navbar I

--- src/app/_components/Navbar.js
+++ src/app/_components/Navbar.js
@@ -1,7 +1,18 @@
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>Hello from Navbar</div>
+    <div className="w-full">
+        <div className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
+            <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
+                <Link href="/">
+                    <span className="flex items-center space-x-2 text-2xl font-medium text-gray-100">
+                        <span>Konstantin Vodolazhskii</span>
+                    </span>
+                </Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
