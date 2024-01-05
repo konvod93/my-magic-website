d5a838b58d7dbfebfe77fcd959ce02298ae4352b
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-01-05 19:07:18 +0200

    updated page - accessinf storyplock from next js

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,11 @@ import ContactSection from "./_components/Contact";
 import FaqSection from "./_components/Faq";
 import Footer from "./_components/Footer";
 
+const getLandingPageData = async () => {
+  const version="draft";
+  const url = `https://api.storyblok.com/v2/cdn/stories/landing-page?version=${version}&token=${token}`;
+  let req = await fetch(url, { cach: "no-store" });
+}
 
 export default function Home() {
   return (
